b455deed7b787b52c61e66f5bbb0d1e75df06fde
agus-develop <agustinfaura@gmail.com>
2020-05-15 20:51:54 +0200

    Base PWA

---
+++ js/sw-utils.js
@@ -0,0 +1,10 @@
+actualizarCcheDinamico = (dynamicCache,req,resp)=>{
+   if(resp.ok){
+    return caches.open(dynamicCache).then(cache=>{
+        cache.put(req,resp.clone() );
+        return resp.clone();
+       });
+   }else{
+       return resp;
+   }
+}
